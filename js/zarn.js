0cb94cec3d6665591697b3ac930c04d2d25eb17e
Robert Babcock <robert@zaphyous.com>
2011-10-01 17:08:06 -0400

    Alpha update with code foundation

---
+++ js/zarn.js
@@ -0,0 +1,17 @@
+// zarn JavaScript Library
+// Author: Robert Babcock
+// Version 1.0 alpha
+
+var zarn = function(){
+	return new zarn.prototype.init();
+};
+
+zarn.prototype = {
+	
+	init: function(){
+		// init the library
+	}
+	
+};
+
+zarn.prototype.init.prototype = zarn.prototype;
