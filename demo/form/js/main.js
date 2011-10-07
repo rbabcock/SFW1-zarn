f31e35002547ae27f9440f49ef62fd341a4b65b1
Robert Babcock <robert@zaphyous.com>
2011-10-07 11:33:15 -0400

    validation example

--- demo/form/js/main.js
+++ demo/form/js/main.js
@@ -1,6 +1,6 @@
 (function(){
 	
-	var formFields = zarn("#myform input");
+	/*var formFields = zarn("#myform input");
 	
 	formFields.each(function(){
 		
@@ -12,6 +12,46 @@
 		"padding": "20px"
 	});
 	
-	zarn("div").removeClass("Testing");
+	zarn("div").removeClass("Testing");*/
+	
+	var validationSet = {
+		email: {
+			test: function(elem){
+				var pattern = /pattern/;
+				return pattern.test(elem.value);
+			},
+			message: "Invalid email address."
+		},
+		phone: {
+			test: function(elem){},
+			message: "Invalid email address."
+		},
+		text: {
+			test: function(elem){},
+			message: "Invalid email address."
+		},
+		website: {
+			test: function(elem){},
+			message: "Invalid email address."
+		},
+	};// end of validation object
+	
+	var regFields = zarn("#myform input");
+	
+	regFields.each(function(){
+		this.onkeyup = function(){
+			for(var key in validationSet){
+				if( zarn(this).hasClass(key) ){
+					if( validationSet[key].test(this) ){
+						console.log("success")
+					}else{
+						console.log("no match")
+						this.style.backgroundColor = "red";
+					};
+				};
+			};
+		};
+	})
+	
 	
 })();
